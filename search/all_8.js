d3a2a14e5cd5fc293ca14b268f445e010d72fe9e
amstokely <amstokely@ucsd.edu>
2023-11-29 08:49:10 -0700

    feat: updated docs.

---
+++ search/all_8.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['chainid_0',['chainId',['../classAtom.html#aa1a479b4d75d05fac5c16fc23e6d2492',1,'Atom']]],
+  ['chunks_1',['Step 3: Load Data in Chunks',['../index.html#step3',1,'']]],
+  ['computation_2',['NetSci: A Toolkit for High Performance Scientific Network Analysis Computation',['../index.html',1,'']]],
+  ['correlationtoadjacency_3',['correlationToAdjacency',['../namespacenetcalc.html#ab56d21e75639a17e01c6a449f67a5a5f',1,'netcalc']]],
+  ['correlationtoadjacencygpu_4',['correlationToAdjacencyGpu',['../namespacenetcalc.html#ad82438a2b54ee9f994cb06fd70454917',1,'netcalc']]],
+  ['counts_5',['counts',['../index.html#step5',1,'Step 5: Increment Neighbor Counts'],['../index.html#step6',1,'Step 6: Update Global Counts']]],
+  ['cuarray_6',['cuarray',['../classCuArray.html',1,'CuArray&lt; T &gt;'],['../classCuArray.html#af20ad601dff695c5e6dc1ed5537b9f1b',1,'CuArray::CuArray()']]],
+  ['cuarray_3c_20float_20_3e_7',['CuArray&lt; float &gt;',['../classCuArray.html',1,'']]],
+  ['cuarrayrow_8',['CuArrayRow',['../classCuArrayRow.html',1,'']]]
+];
---
+++ search/all_c.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['generalizedcorrelation_0',['generalizedcorrelation',['../namespacenetcalc.html#a5af0a49a6a96305d023b16dfe72e1fb8',1,'netcalc::generalizedCorrelation(CuArray&lt; float &gt; *X, CuArray&lt; float &gt; *R, CuArray&lt; int &gt; *ab, int k, int n, int xd, int d, int platform, int checkpointFrequency, std::string checkpointFileName)'],['../namespacenetcalc.html#a5003e2217b6bcaabe332283c884fcd3a',1,'netcalc::generalizedCorrelation(CuArray&lt; float &gt; *X, CuArray&lt; float &gt; *R, CuArray&lt; int &gt; *ab, int k, int n, int xd, int d, int platform)']]],
+  ['generalizedcorrelationcpu_1',['generalizedCorrelationCpu',['../namespacenetcalc.html#acedcac0cffe4f5f00ef08b3cf7201caf',1,'netcalc']]],
+  ['generalizedcorrelationgpu_2',['generalizedCorrelationGpu',['../namespacenetcalc.html#a027a0bbe935ec23e5e506ca908a18c6d',1,'netcalc']]],
+  ['generaterestartabfromcheckpointfile_3',['generateRestartAbFromCheckpointFile',['../namespacenetcalc.html#aa301ee66f48500a31d1b3ed14d3f05f8',1,'netcalc']]],
+  ['get_4',['get',['../classCuArray.html#a3d27d87ef7eac1d2a41189269b1f395d',1,'CuArray']]],
+  ['global_20counts_5',['Step 6: Update Global Counts',['../index.html#step6',1,'']]]
+];
