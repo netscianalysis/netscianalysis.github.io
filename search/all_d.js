001b3cfefc8abf53452463e99a44a5b00a8799e3
amstokely <amstokely@ucsd.edu>
2023-12-15 09:09:33 -0700

    feat: Reverted to commit with working drop down code examples.

--- search/all_d.js
+++ search/all_d.js
@@ -3,9 +3,9 @@ var searchData=
   ['hash_0',['hash',['../classNode.html#a674dc744c03b0b0776f733fe9f28a11d',1,'Node::hash()'],['../classAtom.html#a16f94e10aece5a8100aac271535f4921',1,'Atom::hash()']]],
   ['hedetniemiallshortestpathlengths_1',['hedetniemiAllShortestPathLengths',['../namespacenetcalc.html#a273fb657d5389705f1d40dd1071f238d',1,'netcalc']]],
   ['hedetniemiallshortestpathlengthsgpu_2',['hedetniemiAllShortestPathLengthsGpu',['../namespacenetcalc.html#a03915194208f5f2eacbdbbd59ca53099',1,'netcalc']]],
-  ['hedetniemiallshortestpaths_3',['hedetniemiAllShortestPaths',['../namespacenetcalc.html#a59ec78594882fa96231ac759d243628e',1,'netcalc']]],
-  ['hedetniemiallshortestpathscpu_4',['hedetniemiAllShortestPathsCpu',['../namespacenetcalc.html#a482566ce3f409ff2b87197ad9ebfdca2',1,'netcalc']]],
-  ['hedetniemiallshortestpathsgpu_5',['hedetniemiAllShortestPathsGpu',['../namespacenetcalc.html#a3cef370e68bf260a22b64169af7a606e',1,'netcalc']]],
+  ['hedetniemiallshortestpaths_3',['hedetniemiAllShortestPaths',['../namespacenetcalc.html#abcb4e6f17cf9607067cdb26e2757e141',1,'netcalc']]],
+  ['hedetniemiallshortestpathscpu_4',['hedetniemiAllShortestPathsCpu',['../namespacenetcalc.html#ad6e438f4a43e4d8a32e915668b0af7f1',1,'netcalc']]],
+  ['hedetniemiallshortestpathsgpu_5',['hedetniemiAllShortestPathsGpu',['../namespacenetcalc.html#ac0bf5005b74c6d53436daf970e3d69d5',1,'netcalc']]],
   ['high_20performance_20scientific_20network_20analysis_20computation_6',['NetSci: A Toolkit for High Performance Scientific Network Analysis Computation',['../index.html',1,'']]],
   ['host_7',['host',['../classCuArray.html#a89c635ae8788e21d9a13c5f3ec54d91a',1,'CuArray']]]
 ];
